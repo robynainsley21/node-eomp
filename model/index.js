d7e9a2f5050a0515bfea2ffbd95b1c3fd8494d9c
robynainsley21 <robynainsley2i@gmail.com>
2024-08-19 16:03:13 +0200

    Product Model

--- model/index.js
+++ model/index.js
@@ -1,7 +1,10 @@
 import { Users } from "./Users.js";
+import { Products } from "./Products.js";
 
 const users = new Users();
+const products = new Products();
 
 export {
-    users
+    users,
+    products
 }
