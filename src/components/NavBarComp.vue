797046d82022346cbcc789b58d31add8ee4e4c9e
AneeqBass <aneeqbass@gmail.com>
2024-08-20 14:03:33 +0200

    navbar- added navbar content and functionality

--- src/components/NavBarComp.vue
+++ src/components/NavBarComp.vue
@@ -1,12 +1,36 @@
 <template>
 <div>
-    <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link> |
-    <router-link to="/products">Products</router-link> |
-    <router-link to="/admin">Admin</router-link> |
-    <router-link to="/contact">Contact</router-link>
-  </nav>
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid m-auto">
+    <a class="navbar-brand">Navbar</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav m-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+            <router-link to="/" class="nav-link" >Home</router-link>
+        </li>
+        <li class="nav-item">
+            <router-link to="/about" class="nav-link">About</router-link>
+        </li>
+        <li class="nav-item">
+            <router-link to="/products" class="nav-link" >Products</router-link>
+        </li>
+        <li class="nav-item">
+            <router-link to="/admin" class="nav-link" >Admin</router-link>
+        </li>
+        <li class="nav-item">
+            <router-link to="/contact" class="nav-link" >Contact</router-link>
+        </li>
+      </ul>
+      <form class="d-flex" role="search">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
 </div>  
 </template> 
 
