afc090b48559f0a44be8a098ae9dbe3feaceba22
AneeqBass <aneeqbass@gmail.com>
2024-08-22 12:46:07 +0200

    "Updated store and UserEditView to handle user data instead of product data"

--- src/views/UserEditView.vue
+++ src/views/UserEditView.vue
@@ -1,27 +1,35 @@
 <template>
     <div class="container">
-        <h1>This is an edit product page</h1> 
+        <h1>This is an edit user page</h1> 
 
-        <form id="editProductForm" v-if="product" @submit.prevent="updateProduct">
+        <form id="editUserForm" v-if="user" @submit.prevent="updateUser">
           <div class="mb-3">
-            <label for="editProductName" class="form-label">Product Name</label>
-            <input v-model="product.prodName" type="text" class="form-control text-center" id="editProductName" required>
+            <label for="editUserFirstName" class="form-label">First Name</label>
+            <input v-model="user.firstName" type="text" class="form-control text-center" id="editUserName" required>
           </div>
           <div class="mb-3">
-            <label for="editProductCategory" class="form-label">Category</label>
-            <input v-model="product.category" type="text" class="form-control text-center" id="editProductCategory" required>
+            <label for="editUserLastName" class="form-label">Last Name</label>
+            <input v-model="user.lastName" type="text" class="form-control text-center" id="editUserLastName" required>
           </div>
           <div class="mb-3">
-            <label for="editProductImage" class="form-label">Image URL</label>
-            <input v-model="product.prodURL" type="url" class="form-control text-center" id="editProductImage" required>
+            <label for="editUserAge" class="form-label">Age</label>
+            <input v-model="user.userAge" type="number" class="form-control text-center" id="editUserAge" required>
           </div>
           <div class="mb-3">
-            <label for="editProductQuantity" class="form-label">Quantity</label>
-            <input v-model="product.quantity" type="number" step="0.01" class="form-control text-center" id="editProductQuantity" required>
+            <label for="editUserRole" class="form-label">Role</label>
+            <input v-model="user.userRole" type="text" step="0.01" class="form-control text-center" id="editUserRole" required>
           </div>
           <div class="mb-3">
-            <label for="editProductPrice" class="form-label">Price</label>
-            <input v-model="product.amount" type="number" step="0.01" class="form-control text-center" id="editProductPrice" required>
+            <label for="editUserEmailAdd" class="form-label">Email Address</label>
+            <input v-model="user.emailAdd" type="email" step="0.01" class="form-control text-center" id="editUserEmailAdd" required>
+          </div>
+          <div class="mb-3">
+            <label for="editUserPass" class="form-label">User Password</label>
+            <input v-model="user.userPass" type="text" step="0.01" class="form-control text-center" id="editUserPass" required>
+          </div>
+          <div class="mb-3">
+            <label for="editUserProfile" class="form-label">Profile Pic</label>
+            <input v-model="user.userProfile" type="url" step="0.01" class="form-control text-center" id="editUserProfile" required>
           </div>
           <button type="submit" class="btn btn-primary">Save changes</button>
       </form>
@@ -36,33 +44,30 @@
 import Spinner from '@/components/SpinnerComp.vue'
 
 export default {
-    data(){
-        return{}
-    },
     methods:{
-        fetchProduct(){
-            this.$store.dispatch('fetchProduct', this.$route.params.id)
+        fetchUser(){
+            this.$store.dispatch('fetchUser', this.$route.params.id)
         },
-        updateProduct(){
-            this.$store.dispatch('updateProduct', this.product)
+        updateUser(){
+            this.$store.dispatch('updateUser', this.user)
             .then(() => {
                 this.$router.push('/admin');
             })
             .catch(err => {
-                console.error("Failed to update product:", err);
+                console.error("Failed to update user:", err);
             });
         },
     },
     computed:{
-        product(){
-            return this.$store.state.product;
+        user(){
+            return this.$store.state.user;
         }
     },
     components: {
         Spinner
     },
     mounted() {
-        this.fetchProduct();
+        this.fetchUser();
     }
 }
 </script>
